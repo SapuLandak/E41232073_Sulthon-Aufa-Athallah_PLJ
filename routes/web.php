d3a35f31bbc4a7e3e97818e96220a2fa94b35088
SyapuLandak <thisissulu@gmail.com>
2023-10-26 13:13:02 +0700

    minggu 10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\CobaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,5 +29,16 @@ Route::group(['namespace' => 'App\Http\Controllers\Backend'], function(){
     Route::resource('pengalaman_kerja', 'PengalamanKerjaController');
     Route::resource('pendidikan', 'PendidikanController');
 });
+
+Route::get('/session/create', [SessionController::class,'create']);
+Route::get('/session/show', [SessionController::class,'show']);
+Route::get('/session/delete', [SessionController::class,'delete']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+Route::get('/cobaerror1', [CobaController1::class,'index']);
+Route::get('/cobaerror/{nama?}', [CobaController::class,'index']);
 
 Auth::routes();
