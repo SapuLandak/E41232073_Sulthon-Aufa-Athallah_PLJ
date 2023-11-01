88d877e9e03bda7c2a0240837ec784ddc9322d06
SyapuLandak <thisissulu@gmail.com>
2023-11-01 21:50:39 +0700

    minggu 12-API

--- app/Models/Pendidikan.php
+++ app/Models/Pendidikan.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pendidikan extends Model
-{
+{  
     protected $table = 'pendidikan';
     protected $primaryKey = 'id';
     protected $fillable = [
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\ApiPendidikanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Backend'], function () {
+    Route::get('api_pendidikan', 'ApiPendidikanController@getAll');
+    Route::get('api_pendidikan/{id}', 'ApiPendidikanController@getPen');
+    Route::post('api_pendidikan', 'ApiPendidikanController@createPen');
+    Route::put('api_pendidikan/{id}', 'ApiPendidikanController@updatePen');
+    Route::delete('api_pendidikan/{id}', 'ApiPendidikanController@deletePen');
 });
